3004a3300d861134999e433b525696e0f51cfe14
Adrean Angelov <execthd@gmail.com>
2023-01-19 21:44:13 +0200

    C# Basics Course
    Added all tasks.

---
+++ CSharp-Basics/CSharpBasics-FirstStepsInCoding-Lab/08.PetShop/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace _08.PetShop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int dogsFood = int.Parse(Console.ReadLine());
+            int catsFood = int.Parse(Console.ReadLine());
+
+            double price = dogsFood * 2.5 + catsFood * 4;
+            Console.WriteLine($"{price} lv.");
+        }
+    }
+}
---
+++ CSharp-Basics/CSharpBasics-FirstStepsInCoding-Lab/09.YardGreening/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace _09.YardGreening
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double sqMeters = double.Parse(Console.ReadLine());
+
+            double discount = (sqMeters * 7.61) * 0.18;
+            double finalPrice = (sqMeters * 7.61) - discount;
+
+            Console.WriteLine($"The final price is: {finalPrice:0.00} lv. \nThe discount is: {discount:0.00} lv.");
+        }
+    }
+}
