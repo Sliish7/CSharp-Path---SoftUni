6fcaced337ce6d4b4799e6f5655606dc19e8a7b0
Adrean Angelov <execthd@gmail.com>
2023-01-28 21:41:39 +0200

    C# Basics Course
    Added all tasks.

---
+++ CSharp-Basics/CSharp-Basics-NestedLoops-Exercise/01.NumberPyramid/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace _01.NumberPyramid
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+            int counter = 1;
+
+            for (int col = 1; col <= num; col++)
+            {
+                for (int row = 1; row <= col; row++)
+                {
+                    if (counter > num)
+                    {
+                        break;
+                    }
+                    Console.Write($"{counter} ");
+                    counter++;
+                }
+
+                if (counter > num)
+                {
+                    break;
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
