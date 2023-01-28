5dba2041efadbf4c46f554158a0a3c71e3e43aef
Adrean Angelov <execthd@gmail.com>
2023-01-28 22:02:37 +0200

    ref
    ref

--- CSharp-Basics/CSharp-Basics-NestedLoops-Exercise/02.EqualSumsEvenOddPosition/Program.cs
+++ CSharp-Basics/CSharp-Basics-NestedLoops-Exercise/02.EqualSumsEvenOddPosition/Program.cs
@@ -4,9 +4,34 @@ namespace _02.EqualSumsEvenOddPosition
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            Console.WriteLine("Hello World!");
+            int num1 = int.Parse(Console.ReadLine());
+            int num2 = int.Parse(Console.ReadLine());
+
+            for (int i = num1; i <= num2 ; i++)
+            {
+                string currNum = i.ToString();
+                int odd = 0;
+                int even = 0;
+
+                for (int j = 0; j < currNum.Length; j++)
+                {
+                    int currDigit = int.Parse(currNum[j].ToString());
+
+                    if (j % 2 == 0)
+                    {
+                        even += currDigit;
+                    }
+                    else
+                        odd += currDigit;
+                }
+
+                if (odd == even)
+                {
+                    Console.WriteLine(i);
+                }
+            }
         }
     }
 }
