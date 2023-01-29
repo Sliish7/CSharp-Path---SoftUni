66fcfbc0fe26f485e0e1bbb2aa4ebe0ecf4d94ab
Adrean Angelov <execthd@gmail.com>
2023-01-29 10:35:39 +0200

    C# Basics Course
    Added all tasks.

---
+++ CSharp-Basics/CSharp-Basics-NestedLoops-Exercise/04.TrainTheTrainers/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace _04.TrainTheTrainers
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            int judges = int.Parse(Console.ReadLine());
+            string input = Console.ReadLine();
+            int subjectCount = 0;
+            double finalAsess = 0;
+            while (input != "Finish")
+            {        
+                double avgGrade = 0;
+                for (int i = 1; i <= judges; i++)
+                {
+                    double grade = double.Parse(Console.ReadLine());
+                    avgGrade += grade;
+                }
+                avgGrade = avgGrade/ judges;
+                subjectCount++;
+                finalAsess += avgGrade;
+
+                Console.WriteLine($"{input} - {avgGrade:f2}.");
+                input = Console.ReadLine();
+            }
+            finalAsess = finalAsess / subjectCount;
+            Console.WriteLine($"Student's final assessment is {finalAsess:f2}.");
+        }
+    }
+}
