5f09377d4f570829b16fb86966a595bcb1f6bac9
Adrean Angelov <execthd@gmail.com>
2023-01-31 17:35:19 +0200

    C# Fundamentals Course

---
+++ CSharp-Fundamentals/BasicSyntax-Conditional-Statements-and-Loops-Lab/09.SumOfOddNumbers/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _09.SumOfOddNumbers
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            int input = int.Parse(Console.ReadLine());
+            int sum = 0;
+
+            for (int i = 1; i <= input * 2; i+= 2)
+            {
+                Console.WriteLine(i);
+                sum += i;
+            }
+            Console.WriteLine($"Sum: {sum}");
+        }
+    }
+}
