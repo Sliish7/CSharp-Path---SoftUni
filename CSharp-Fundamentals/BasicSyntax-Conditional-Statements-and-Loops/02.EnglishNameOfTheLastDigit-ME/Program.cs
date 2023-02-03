eaf88385b5f323e0b702822fa610875b6cf2eeff
Adrean Angelov <execthd@gmail.com>
2023-02-03 19:40:50 +0200

    C# Fundamentals Course
    More Exercise problems

---
+++ CSharp-Fundamentals/BasicSyntax-Conditional-Statements-and-Loops/02.EnglishNameOfTheLastDigit-ME/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace _02.EnglishNameOfTheLastDigit_ME
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+            string numAsStr = num.ToString();
+
+            for (int i = numAsStr.Length - 1; i < numAsStr.Length; i++)
+            {
+                char digit = (char)numAsStr[i];
+                int intDigit = digit - '0';
+                string output = Spelling(intDigit);
+                Console.WriteLine(output);
+            }
+        }
+
+        static string Spelling(int intDigit)
+        {
+            return intDigit switch
+            {
+                0 => "zero",
+                1 => "one",
+                2 => "two",
+                3 => "three",
+                4 => "four",
+                5 => "five",
+                6 => "six",
+                7 => "seven",
+                8 => "eight",
+                9 => "nine",
+                _ => "",
+            };
+        }
+    }
+}
+}
