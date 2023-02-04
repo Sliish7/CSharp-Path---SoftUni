2e9dc2491da92cca85c428cba724454cc6f4774a
Adrean Angelov <execthd@gmail.com>
2023-02-04 05:19:15 +0200

    C# Fundamentals Course

---
+++ CSharp-Fundamentals/DataTypesAndVariables-Lab/01.ConvertMetersToKilometers/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace _01.ConvertMetersToKilometers
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            double meters = int.Parse(Console.ReadLine());
+            double km = meters / 1000;
+            Console.WriteLine($"{km:f2}");
+        }
+    }
+}
