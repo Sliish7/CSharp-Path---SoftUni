1092a500827204efb5029759851c10683c1cdc18
Adrean Angelov <execthd@gmail.com>
2023-02-05 18:11:49 +0200

    C# Fundamentals Course

---
+++ CSharp-Fundamentals/DataTypesAndVariables-Lab/06.ReversedChars/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _06.ReversedChars
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            string[] arr = new string[3];
+
+            for (int i = 0; i < 3; i++)
+            {
+                arr[i] = Console.ReadLine();
+            }
+            Array.Reverse(arr);
+
+            foreach (string el in arr)
+            {
+                Console.Write($"{el} ");
+            }
+        }
+    }
+}
