e20142e83e836fa6e1d0bdd9c0525c8914f4f437
Adrean Angelov <execthd@gmail.com>
2023-02-06 15:51:30 +0200

    C# Fundamentals Course

---
+++ CSharp-Fundamentals/DataTypesAndVariables-Exercise/07.WaterOverflow/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace _07.WaterOverflow
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            int input = int.Parse(Console.ReadLine());
+            int capacity = 255;
+            int totalLitres = 0;
+
+            for (int i = 1; i <= input; i++)
+            {
+                int litres = int.Parse(Console.ReadLine());
+
+                if (litres > capacity)
+                {
+                    Console.WriteLine("Insufficient capacity!");
+                }
+                else
+                {
+                    capacity -= litres;
+                    totalLitres += litres;
+                }
+            }
+            Console.WriteLine(totalLitres);
+        }
+    }
+}
