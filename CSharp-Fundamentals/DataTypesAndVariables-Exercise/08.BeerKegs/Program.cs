e11e26ad43a010db1c1ef9c0d21c9439e029ff89
Adrean Angelov <execthd@gmail.com>
2023-02-06 17:16:04 +0200

    C# Fundamentals Course

---
+++ CSharp-Fundamentals/DataTypesAndVariables-Exercise/08.BeerKegs/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace _08.BeerKegs
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            int kegsCount = int.Parse(Console.ReadLine());
+            string biggestKeg = "";
+            double biggestV = 0;
+
+            for (int i = 1; i <= kegsCount; i++)
+            {
+                string model = Console.ReadLine();
+                float radius = float.Parse(Console.ReadLine());
+                int height = int.Parse(Console.ReadLine());
+
+                double V = Math.PI * Math.Pow(radius, 2) * height;
+                if (V > biggestV)
+                {
+                    biggestV = V;
+                    biggestKeg = model;
+                }
+            }
+            Console.WriteLine(biggestKeg);
+        }
+    }
+}
