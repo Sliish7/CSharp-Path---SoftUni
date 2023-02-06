2353185a922bf8197761459d12d08e77d05a8c46
Adrean Angelov <execthd@gmail.com>
2023-02-06 19:29:11 +0200

    C# Fundamentals Course

---
+++ CSharp-Fundamentals/DataTypesAndVariables-Exercise/10.PokeMon/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace _10.PokeMon
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            int power = int.Parse(Console.ReadLine());
+            int distance = int.Parse(Console.ReadLine());
+            int exhaust = int.Parse(Console.ReadLine());
+
+            int remainingPower = power;
+            int targetCount = 0;
+
+            while (remainingPower >= distance)
+            {
+                remainingPower -= distance;
+                targetCount++;
+
+                if (power / 2 == remainingPower)
+                {
+                    remainingPower /= exhaust;
+                }
+            }
+
+            Console.WriteLine(remainingPower);
+            Console.WriteLine(targetCount);
+        }
+    }
+}
