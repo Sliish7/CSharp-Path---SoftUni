89483e8a5dc14f9fab881ce980ac2461f96e503d
Adrean Angelov <execthd@gmail.com>
2023-02-06 14:26:32 +0200

    C# Fundamentals Course

---
+++ CSharp-Fundamentals/DataTypesAndVariables-Exercise/03.Elevator/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace _03.Elevator
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            int people = int.Parse(Console.ReadLine());
+            int capacity = int.Parse(Console.ReadLine());
+
+            int coursesNeeded = 0;
+
+            while (people > 0)
+            {
+                people -= capacity;
+                coursesNeeded++;
+            }
+
+            if (coursesNeeded == 1)
+            {
+                Console.WriteLine("All the persons fit inside in the elevator.\nOnly one course is needed.");
+            }
+            else
+                Console.WriteLine(coursesNeeded);
+        }
+    }
+}
