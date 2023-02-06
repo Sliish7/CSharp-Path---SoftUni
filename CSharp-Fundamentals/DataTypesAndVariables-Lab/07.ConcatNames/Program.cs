5067d1c542d730ea06c242bb55c0cd676523988d
Adrean Angelov <execthd@gmail.com>
2023-02-06 13:19:33 +0200

    C# Fundamentals Course

---
+++ CSharp-Fundamentals/DataTypesAndVariables-Lab/07.ConcatNames/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace _07.ConcatNames
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            string firstName = Console.ReadLine();
+            string secondName = Console.ReadLine();
+            string delimeter = Console.ReadLine();
+
+            Console.WriteLine(String.Join(delimeter, firstName, secondName));
+        }
+    }
+}
