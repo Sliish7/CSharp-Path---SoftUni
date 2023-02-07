63d34b429cf619d23ffa344dc531d7809a061b28
Adrean Angelov <execthd@gmail.com>
2023-02-07 20:47:32 +0200

    C# Fundamentals Course
    + More Exercise problems added

---
+++ CSharp-Fundamentals/DataTypesAndVariables-Exercise/04.RefactoringPrimeChecker-ME/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _04.RefactoringPrimeChecker_ME
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            int range = int.Parse(Console.ReadLine());
+            for (int i = 2; i <= range; i++)
+            {
+                bool isPrime = true;
+                for (int j = 2; j < i; j++)
+                {
+                    if (i % j == 0)
+                    {
+                        isPrime = false;
+                        break;
+                    }
+                }
+                Console.WriteLine("{0} -> {1}", i, isPrime.ToString().ToLower());
+            }
+
+        }
+    }
+}
