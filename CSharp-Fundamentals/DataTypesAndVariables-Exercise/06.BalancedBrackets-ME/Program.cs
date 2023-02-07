011e24658887141c31fdc3822704e03591a3632b
Adrean Angelov <execthd@gmail.com>
2023-02-07 22:39:06 +0200

    C# Fundamentals Course
    + More Exercise problems added

---
+++ CSharp-Fundamentals/DataTypesAndVariables-Exercise/06.BalancedBrackets-ME/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace _06.BalancedBrackets_ME
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            int lines = int.Parse(Console.ReadLine());
+            bool isBalanced = true;
+            bool isOpened = false;
+
+            for (int i = 1; i <= lines; i++)
+            {
+                string input = Console.ReadLine();
+
+                if (isOpened && input == "(" || !isOpened && input == ")")
+                {
+                    isBalanced = false;
+                    break;
+                }
+                if (isOpened && input == "(")
+                {
+                    isBalanced = false;
+                    break;
+                }
+
+                if (input == "(")
+                {
+                    isOpened = true;
+                    isBalanced = false;
+                }
+                else if (input == ")")
+                {
+                    isOpened = false;
+                    isBalanced = true;
+                }
+            }
+
+            if (isBalanced == true)
+            {
+                Console.WriteLine("BALANCED");
+            }
+            else
+                Console.WriteLine("UNBALANCED");
+        }
+    }
+}
