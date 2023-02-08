03e29681eeb481a092230f9c835be6278e95fdc7
Adrean Angelov <execthd@gmail.com>
2023-02-08 20:53:28 +0200

    C# Fundamentals Course

---
+++ CSharp-Fundamentals/Arrays-Lab/02.PrintNumbersInReverse/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace _02.PrintNumbersInReverse
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            int range = int.Parse(Console.ReadLine());
+            int[] numbers = new int[range];
+
+            for (int i = 0; i < range; i++)
+            {
+                int currNum = int.Parse(Console.ReadLine());
+                numbers[i] = currNum;
+            }
+            Array.Reverse(numbers);
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                Console.Write($"{numbers[i]} ");
+            }
+
+            //foreach (int num in numbers)
+            //{
+            //    Console.Write($"{num} ");
+            //}
+        }
+    }
+}
