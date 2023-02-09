559a4728bf42e5a8747a1fd7cda0edf3a5e89c04
Adrean Angelov <execthd@gmail.com>
2023-02-09 17:25:00 +0200

    C# Fundamentals Course

---
+++ CSharp-Fundamentals/Arrays-Lab/03.RoundingNumbers/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _03.RoundingNumbers
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            string[] input = Console.ReadLine().Split();
+
+            foreach (string element in input)
+            {
+                double currNum = double.Parse(element);
+
+                string result = $"{currNum} => {Math.Round(currNum, MidpointRounding.AwayFromZero)}";
+                Console.WriteLine(result);
+            }
+        }
+    }
+}
