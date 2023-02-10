bc6b43c1b364b4368feb9bfbf6f9529ec61d705c
Adrean Angelov <execthd@gmail.com>
2023-02-10 22:31:38 +0200

    C# Fundamentals Course

---
+++ CSharp-Fundamentals/Arrays-Lab/05.SumEvenNumbers/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _05.SumEvenNumbers
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            string[] numbers = Console.ReadLine().Split();
+            int sum = 0;
+
+            foreach (var number in numbers)
+            {
+                int currNum = int.Parse(number);
+
+                if (currNum % 2 == 0)
+                {
+                    sum += currNum;
+                }
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
