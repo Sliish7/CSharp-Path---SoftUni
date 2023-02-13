c35de12fc3ff228baa5376d1c18a023c5742e891
Adrean Angelov <execthd@gmail.com>
2023-02-13 16:26:42 +0200

    C# Fundamentals Course

---
+++ CSharp-Fundamentals/Arrays-Exercise/04.ArrayRotation/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace _04.ArrayRotation
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            int[] numbers = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+            Queue<int> numbersQueue = new Queue<int>(numbers);
+            int moveCount = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < moveCount; i++)
+            {
+                int currNum = numbersQueue.Dequeue();
+                numbersQueue.Enqueue(currNum);
+            }
+
+            Console.WriteLine(string.Join(" ", numbersQueue));
+        }
+    }
+}
