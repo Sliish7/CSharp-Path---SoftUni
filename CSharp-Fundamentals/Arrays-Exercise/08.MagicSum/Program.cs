485d283d02d6492f5881de17c0747fc89a05772b
Adrean Angelov <execthd@gmail.com>
2023-02-16 11:49:24 +0200

    C# Fundamentals Course

---
+++ CSharp-Fundamentals/Arrays-Exercise/08.MagicSum/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Runtime.ExceptionServices;
+
+namespace _08.MagicSum
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            int[] numbers = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+            int magicNum = int.Parse(Console.ReadLine());
+            int firstNum = 0; int secondNum = 0;
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                firstNum = numbers[i];
+
+                for (int j = numbers.Length - 1; j > i; j--)
+                {
+                    int totalSum = 0;
+                    if (j != i)
+                    {
+                        secondNum = numbers[j];
+                        totalSum = firstNum + secondNum;
+
+                        if (totalSum == magicNum )
+                        {
+                            Console.WriteLine($"{numbers[i]} {numbers[j]}");
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
