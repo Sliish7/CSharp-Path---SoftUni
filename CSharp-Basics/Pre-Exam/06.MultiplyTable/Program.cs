6e285c0462f1dd4d8b67ee1432d1886e0d1f8f96
Adrean Angelov <execthd@gmail.com>
2023-02-19 12:42:00 +0200

    C# Basics Course
    Programming Basics Online Pre - Exam - 18 and 19 February 2023

---
+++ CSharp-Basics/Pre-Exam/06.MultiplyTable/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Globalization;
+
+namespace _06.MultiplyTable
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            int number = int.Parse(Console.ReadLine());
+            int firstDig = number / 100;
+            int secondDig = (number / 10) % 10;
+            int thirdDig = number % 10;
+
+            for (int i = 1; i <= thirdDig; i++)
+            {
+                for (int j = 1; j <= secondDig; j++)
+                {
+                    for (int n = 1; n <= firstDig; n++)
+                    {
+                        int result = i * j * n;
+                        Console.WriteLine($"{i} * {j} * {n} = {result}");
+                    }
+                }
+            }
+            
+        }
+    }
+}
