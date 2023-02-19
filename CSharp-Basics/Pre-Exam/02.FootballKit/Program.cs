2260e3561f24a359eb23ace192acbbdceec2bcdb
Adrean Angelov <execthd@gmail.com>
2023-02-19 11:32:56 +0200

    C# Basics Course
    Programming Basics Online Pre - Exam - 18 and 19 February 2023

---
+++ CSharp-Basics/Pre-Exam/02.FootballKit/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace _02.FootballKit
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            double tshirtPrice = double.Parse(Console.ReadLine());
+            double sumNeeded = double.Parse(Console.ReadLine());
+            double shortsPrice = tshirtPrice * 0.75;
+            double socksPrice = shortsPrice * 0.20;
+            double shoesPrice = 2 * (tshirtPrice + shortsPrice);
+            double totalPrice = tshirtPrice + socksPrice + shortsPrice + shoesPrice;
+            totalPrice -= totalPrice * 0.15;
+
+            if (totalPrice >= sumNeeded)
+            {
+                Console.WriteLine("Yes, he will earn the world - cup replica ball!");
+                Console.WriteLine($"His sum is {totalPrice:f2} lv.");
+            }
+            else
+            {
+                
+                Console.WriteLine("No, he will not earn the world-cup replica ball.");
+                Console.WriteLine($"He needs {sumNeeded - totalPrice:f2} lv. more.");
+            }
+        }
+    }
+}
