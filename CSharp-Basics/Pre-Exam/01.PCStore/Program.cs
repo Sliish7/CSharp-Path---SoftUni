5caf33f6d1c92a16c668020399e22904f4cbc67d
Adrean Angelov <execthd@gmail.com>
2023-02-19 11:19:47 +0200

    C# Basics Course
    Programming Basics Online Pre - Exam - 18 and 19 February 2023

---
+++ CSharp-Basics/Pre-Exam/01.PCStore/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _01.PCStore
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            double cpuPrice = double.Parse(Console.ReadLine());
+            double gpuPrice = double.Parse(Console.ReadLine());
+            double ramPrice = double.Parse(Console.ReadLine());
+            double quantityRam = double.Parse(Console.ReadLine());
+            double discount = double.Parse(Console.ReadLine());
+            cpuPrice *= 1.57;
+            gpuPrice *= 1.57;
+            ramPrice = (ramPrice * 1.57) * quantityRam;
+            cpuPrice -= cpuPrice * discount; gpuPrice -= gpuPrice* discount;
+
+            double totalPrice = cpuPrice + gpuPrice + ramPrice;
+
+            Console.WriteLine($"Money needed - {totalPrice:f2} leva.");
+        }
+    }
+}
