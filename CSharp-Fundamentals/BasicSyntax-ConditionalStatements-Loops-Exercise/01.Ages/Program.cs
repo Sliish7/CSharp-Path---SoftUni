fd37e968b516f8a4de4ddb3d7aa6b8fe08ee7f6d
Adrean Angelov <execthd@gmail.com>
2023-05-17 19:20:14 +0300

    C# Fundamentals Course

--- CSharp-Fundamentals/BasicSyntax-ConditionalStatements-Loops-Exercise/01.Ages/Program.cs
+++ CSharp-Fundamentals/BasicSyntax-ConditionalStatements-Loops-Exercise/01.Ages/Program.cs
@@ -2,9 +2,31 @@
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            Console.WriteLine("Hello, World!");
+            int age = int.Parse(Console.ReadLine());
+            string person;
+
+            if (age < 3)
+            {
+                person = "baby";
+            }
+            else if (age < 14)
+            {
+                person = "child";
+            }
+            else if (age < 20)
+            {
+                person = "teenager";
+            }
+            else if (age < 66)
+            {
+                person = "adult";
+            }
+            else
+                person = "elder";
+
+            Console.WriteLine(person);
         }
     }
 }
---
+++ CSharp-Fundamentals/BasicSyntax-ConditionalStatements-Loops-Exercise/03.Vacation/Program.cs
@@ -0,0 +1,87 @@
+﻿namespace _03.Vacation
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            int people = int.Parse(Console.ReadLine());
+            string typeOfVac = Console.ReadLine();
+            string dayOfTheWeek = Console.ReadLine();
+            double price = 0; ; double totalPrice = 0;
+
+            switch (typeOfVac)
+            {
+                case "Students":                    
+
+                    if (dayOfTheWeek == "Friday")
+                    {
+                        price = 8.45;
+                    }
+                    else if (dayOfTheWeek == "Saturday")
+                    {
+                        price = 9.80;
+                    }
+                    else if (dayOfTheWeek == "Sunday")
+                    {
+                        price = 10.46;
+                    }
+
+                    totalPrice = people * price;
+
+                    if (people >= 30)
+                    {
+                        totalPrice *= 0.85;
+                    }
+
+                    break;
+                case "Business":
+
+                    if (dayOfTheWeek == "Friday")
+                    {
+                        price = 10.90;
+                    }
+                    else if (dayOfTheWeek == "Saturday")
+                    {
+                        price = 15.60;
+                    }
+                    else if (dayOfTheWeek == "Sunday")
+                    {
+                        price = 16;
+                    }
+
+                    if (people < 100)
+                    {
+                        totalPrice = people * price;
+                    }
+                    else
+                        totalPrice = (people - 10) * price;
+
+                    break;
+                case "Regular":
+
+                    if (dayOfTheWeek == "Friday")
+                    {
+                        price = 15;
+                    }
+                    else if (dayOfTheWeek == "Saturday")
+                    {
+                        price = 20;
+                    }
+                    else if (dayOfTheWeek == "Sunday")
+                    {
+                        price = 22.50;
+                    }
+
+                    totalPrice = people * price;
+
+                    if (people >= 10 && people <= 20)
+                    {
+                        totalPrice *= 0.95;
+                    }
+                    break;
+            }
+
+            Console.WriteLine($"Total price: {totalPrice:f2}");
+        }
+    }
+}
