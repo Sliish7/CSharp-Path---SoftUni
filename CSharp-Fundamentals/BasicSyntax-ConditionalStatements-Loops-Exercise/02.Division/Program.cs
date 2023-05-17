ced7e472b70f336910d49d65d926d87de8d319be
Adrean Angelov <execthd@gmail.com>
2023-05-17 19:29:13 +0300

    C# Fundamentals Course

--- CSharp-Fundamentals/BasicSyntax-ConditionalStatements-Loops-Exercise/02.Division/Program.cs
+++ CSharp-Fundamentals/BasicSyntax-ConditionalStatements-Loops-Exercise/02.Division/Program.cs
@@ -2,9 +2,35 @@
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            Console.WriteLine("Hello, World!");
+            int number = int.Parse(Console.ReadLine());
+            string output = "The number is divisible by ";
+
+
+            if (number % 10 == 0)
+            {
+                output += "10";
+            }
+            else if (number % 7 == 0)
+            {
+                output += "7";
+            }
+            else if (number % 6 == 0)
+            {
+                output += "6";
+            }
+            else if (number % 3 == 0)
+            {
+                output += "3";
+            }
+            else if (number % 2 == 0)
+            {
+                output += "2";
+            }
+            else
+                output = "Not divisible";
+            Console.WriteLine(output);
         }
     }
 }
