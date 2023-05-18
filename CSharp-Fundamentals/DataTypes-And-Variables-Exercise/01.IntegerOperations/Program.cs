9cd031bea1991c2b91c36fc71b04411e82f4dc5c
Adrean Angelov <execthd@gmail.com>
2023-05-19 00:23:07 +0300

    C# Fundamentals Course

---
+++ CSharp-Fundamentals/DataTypes-And-Variables-Exercise/01.IntegerOperations/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace _01.IntegerOperations
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            int n1 = int.Parse(Console.ReadLine());
+            int n2 = int.Parse(Console.ReadLine());
+            int n3 = int.Parse(Console.ReadLine());
+            int n4 = int.Parse(Console.ReadLine());
+
+            int result = ((n1 + n2) / n3) * n4;
+            Console.WriteLine(result);
+        }
+    }
+}
---
+++ CSharp-Fundamentals/DataTypes-And-Variables-Exercise/02.SumDigits/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace _02.SumDigits
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            string number = Console.ReadLine();
+            int result = 0;
+
+            char[] charArray = number.ToCharArray();
+
+            for (int i = 0; i < charArray.Length; i++)
+            {
+                int num = charArray[i] - '0';
+                result += num;
+            }
+            Console.WriteLine(result);
+        }
+    }
+}
