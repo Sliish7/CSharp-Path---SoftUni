e2d1dd644d1d7fe9dc5f314230b76eec97d028a0
Adrean Angelov <execthd@gmail.com>
2023-05-18 17:36:18 +0300

    C# Fundamentals Course

---
+++ CSharp-Fundamentals/BasicSyntax-ConditionalStatements-Loops-Exercise/08.TriangleOfNumbers/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace _08.TriangleOfNumbers
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            int number = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= number; i++)
+            {
+                int currNum = i;
+                for (int j = 1; j <= currNum; j++)
+                {
+                    if (currNum > j)
+                    {
+                        Console.Write($"{currNum} ");
+                    }
+                    else
+                        Console.Write($"{currNum}\n");
+
+                }
+            }
+        }
+    }
+}
