5fe9f958ac0cca2f6906d89d0df026cfcbf94807
Adrean Angelov <execthd@gmail.com>
2023-05-19 00:58:05 +0300

    C# Fundamentals Course

---
+++ CSharp-Fundamentals/DataTypes-And-Variables-Exercise/05.PrintPart-ASCII-Table/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace _05.PrintPart_ASCII_Table
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            int start = int.Parse(Console.ReadLine());
+            int end = int.Parse(Console.ReadLine());
+
+            for (int i = start; i <= end; i++)
+            {
+                char currASCII = (char)i;
+                if (i != end)
+                {
+                    Console.Write(currASCII + " ");
+                }
+                else
+                    Console.Write(currASCII);
+            }
+        }
+    }
+}
