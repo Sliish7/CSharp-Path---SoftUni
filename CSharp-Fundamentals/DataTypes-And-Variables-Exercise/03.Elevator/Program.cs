8445ee9b63b65392a9581452ddcc526c9bc4600b
Adrean Angelov <execthd@gmail.com>
2023-05-19 00:46:33 +0300

    C# Fundamentals Course

---
+++ CSharp-Fundamentals/DataTypes-And-Variables-Exercise/03.Elevator/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace _03.Elevator
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            int people = int.Parse(Console.ReadLine());
+            int capacity = int.Parse(Console.ReadLine());
+            int cources = (int)Math.Ceiling((double)people / capacity);
+
+            Console.WriteLine(cources);
+        }
+    }
+}
---
+++ CSharp-Fundamentals/DataTypes-And-Variables-Exercise/04.SumOfChars/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace _04.SumOfChars
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            int charCount = int.Parse(Console.ReadLine());
+            int result = 0;
+
+            for (int i = 0; i < charCount; i++)
+            {
+                char letter = char.Parse(Console.ReadLine());
+                result += letter;               
+            }
+            Console.WriteLine($"The sum equals: {result}");
+        }
+    }
+}
