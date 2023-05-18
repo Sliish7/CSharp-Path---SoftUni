3b263f44970647cfe42fe2fde1f218bc02b57dfb
Adrean Angelov <execthd@gmail.com>
2023-05-18 19:38:48 +0300

    C# Fundamentals Course

---
+++ CSharp-Fundamentals/BasicSyntax-ConditionalStatements-Loops-Exercise/11.Orders/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace _11.Orders
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            int orders = int.Parse(Console.ReadLine());
+            double totalPrice = 0;
+
+            while (orders != 0)
+            {
+                double capsulePrice = double.Parse(Console.ReadLine());
+                int days = int.Parse(Console.ReadLine());
+                int capsuleCount = int.Parse(Console.ReadLine());
+
+                double coffeePrice = ((days * capsuleCount) * capsulePrice);
+                totalPrice+= coffeePrice;
+                Console.WriteLine($"The price for the coffee is: ${coffeePrice:f2}");
+
+                orders--;
+            }
+
+            Console.WriteLine($"Total: ${totalPrice:f2}");
+        }
+    }
+}
