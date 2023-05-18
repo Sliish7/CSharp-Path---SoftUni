ebb3ecb283851dc8cba66c25cef53c652c401162
Adrean Angelov <execthd@gmail.com>
2023-05-18 19:45:03 +0300

    C# Fundamentals Course

---
+++ CSharp-Fundamentals/BasicSyntax-ConditionalStatements-Loops-Lab/11.MultiplicationTable2.0/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _11.MultiplicationTable2._0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int range = int.Parse(Console.ReadLine());
+
+            if (range <= 10)
+            {
+                for (int i = range; i <= 10; i++)
+                {
+                    int res = n * i;
+                    Console.WriteLine($"{n} X {i} = {res}");
+                }
+            }
+            else
+                Console.WriteLine($"{n} X {range} = {n * range}");
+
+        }
+    }
+}
