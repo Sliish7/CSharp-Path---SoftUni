14ecef57e7159c6e407b05502a34007e3e45ddb6
Adrean Angelov <execthd@gmail.com>
2023-05-18 22:40:34 +0300

    C# Fundamentals Course

---
+++ CSharp-Fundamentals/BasicSyntax-ConditionalStatements-Loops-MoreExercise/02.EnglishNameLastDigit/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace _02.EnglishNameLastDigit
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            string number = Console.ReadLine();
+            int lastDig = 0;
+            string spelling = "zz";
+
+            char[] digitArray = number.ToCharArray();
+
+            for (int i = 0; i < digitArray.Length; i++)
+            {
+                if (i == digitArray.Length - 1)
+                {
+                    lastDig = digitArray[i] - '0';
+                }
+            }
+
+            switch (lastDig)
+            {
+                case 0: spelling = "zero"; break;
+                case 1: spelling = "one"; break;
+                case 2: spelling = "two"; break;
+                case 3: spelling = "three"; break;
+                case 4: spelling = "four"; break;
+                case 5: spelling = "five"; break;
+                case 6: spelling = "six"; break;
+                case 7: spelling = "seven"; break;
+                case 8: spelling = "eight"; break;
+                case 9: spelling = "nine"; break;
+                default:break;
+            }
+
+            Console.WriteLine(spelling);
+        }
+    }
+}
---
+++ CSharp-Fundamentals/BasicSyntax-ConditionalStatements-Loops-MoreExercise/03.GamingStore/Program.cs
@@ -0,0 +1,56 @@
+﻿namespace _03.GamingStore
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            double balance = double.Parse(Console.ReadLine());
+            double currBalance = balance;
+
+            string game;
+
+            while (currBalance >= 0)
+            {
+                game = Console.ReadLine();
+
+                if (game == "Game Time")
+                {
+                    break;
+                }
+
+                double gamePrice = 0;
+
+                switch (game)
+                {
+                    case "OutFall 4": gamePrice = 39.99;break;
+                    case "CS: OG": gamePrice = 15.99; break;
+                    case "Zplinter Zell": gamePrice = 19.99; break;
+                    case "Honored 2": gamePrice = 59.99; break;
+                    case "RoverWatch": gamePrice = 29.99; break;
+                    case "RoverWatch Origins Edition": gamePrice = 39.99; break;
+                    default:break;
+                }
+
+                if (gamePrice != 0)
+                {
+                    if (currBalance >= gamePrice)
+                    {
+                        Console.WriteLine($"Bought {game}");
+                        currBalance -= gamePrice;
+                    }
+                    else
+                        Console.WriteLine("Too Expensive");
+                }
+                else
+                    Console.WriteLine("Not Found");
+            }
+
+            if (currBalance == 0.00)
+            {
+                Console.WriteLine("Out of money!");
+            }
+            else
+                Console.WriteLine($"Total spent: ${Math.Abs(balance - currBalance):f2}. Remaining: ${currBalance:f2}");
+        }
+    }
+}
