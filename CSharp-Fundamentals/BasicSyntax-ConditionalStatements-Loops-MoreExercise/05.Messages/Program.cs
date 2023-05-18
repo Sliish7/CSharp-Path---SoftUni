31fd346118e174a118fc570ea8d341ca00a0aa76
Adrean Angelov <execthd@gmail.com>
2023-05-18 22:53:10 +0300

    C# Fundamentals Course

--- CSharp-Fundamentals/BasicSyntax-ConditionalStatements-Loops-MoreExercise/05.Messages/Program.cs
+++ CSharp-Fundamentals/BasicSyntax-ConditionalStatements-Loops-MoreExercise/05.Messages/Program.cs
@@ -2,9 +2,57 @@
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            Console.WriteLine("Hello, World!");
+            int numOfLett = int.Parse(Console.ReadLine());
+            int wordCount = 0;
+            string sentence = "";
+
+            while (wordCount != numOfLett)
+            {
+                int letter = int.Parse(Console.ReadLine());
+
+                sentence += Letter(letter);
+                wordCount++;
+            }
+
+            Console.WriteLine(sentence);
+        }
+
+        static string Letter(int letter)
+        {
+            return letter switch
+            {
+                2 => "a",
+                22 => "b",
+                222 => "c",
+                3 => "d",
+                33 => "e",
+                333 => "f",
+                4 => "g",
+                44 => "h",
+                444 => "i",
+                5 => "j",
+                55 => "k",
+                555 => "l",
+                6 => "m",
+                66 => "n",
+                666 => "o",
+                7 => "p",
+                77 => "q",
+                777 => "r",
+                7777 => "s",
+                8 => "t",
+                88 => "u",
+                888 => "v",
+                9 => "w",
+                99 => "x",
+                999 => "y",
+                9999 => "z",
+                0 => " ",
+                _ => ""
+            };
+
         }
     }
 }
