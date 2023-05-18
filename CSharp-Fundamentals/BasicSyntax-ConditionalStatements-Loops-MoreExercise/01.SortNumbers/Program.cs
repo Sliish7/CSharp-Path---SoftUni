347929b9d03b6a0abba1e0d0edcd5ada94e48690
Adrean Angelov <execthd@gmail.com>
2023-05-18 20:10:18 +0300

    C# Fundamentals Course

---
+++ CSharp-Fundamentals/BasicSyntax-ConditionalStatements-Loops-MoreExercise/01.SortNumbers/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace _01.SortNumbers
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            int[] arrNum = {0, 0, 0};
+
+            for (int i = 0; i < 3; i++)
+            {
+                int num = int.Parse(Console.ReadLine());
+                arrNum[i] = num;
+            }
+
+            for (int i = 0; i < arrNum.Length; i++)
+            {
+                Array.Sort(arrNum);
+                Array.Reverse(arrNum);
+                Console.WriteLine(arrNum[i]);
+            }
+
+        }
+    }
+}
