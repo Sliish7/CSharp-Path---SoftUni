50170a402cd609d01ac3d2ddb2b26ce4d04b6d24
Adrean Angelov <execthd@gmail.com>
2023-05-18 22:49:53 +0300

    C# Fundamentals Course

---
+++ CSharp-Fundamentals/BasicSyntax-ConditionalStatements-Loops-MoreExercise/04.ReverseString/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace _04.ReverseString
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            string word = Console.ReadLine();
+
+            char[] charArray = word.ToCharArray();
+            Array.Reverse(charArray);
+
+            for (int i = 0; i < charArray.Length; i++)
+            {
+                Console.Write(charArray[i]);
+            }
+        }
+    }
+}
