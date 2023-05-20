49814ce17939b87725c1af7cd551b0205cdefef1
Adrean Angelov <execthd@gmail.com>
2023-05-20 21:14:42 +0300

    C# Fundamentals Course

---
+++ CSharp-Fundamentals/DataTypes-And-Variables-Lab/12.RefactorSpecialNumbers/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace _12.RefactorSpecialNumbers
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            int range = int.Parse(Console.ReadLine());
+            int total = 0;
+            for (int i = 1; i <= range; i++)
+            {
+                int num = i;
+                while (i > 0)
+                {
+                    total += i % 10;
+                    i = i / 10;
+                }
+                bool isSpecial = (total == 5) || (total == 7) || (total == 11);
+                Console.WriteLine("{0} -> {1}", num, isSpecial);
+                total = 0;
+                i = num;
+            }
+        }
+    }
+}
