52e53fc6322413288fafe4a26d151714a1264f87
Adrean Angelov <execthd@gmail.com>
2023-05-20 20:29:22 +0300

    C# Fundamentals Course

---
+++ CSharp-Fundamentals/DataTypes-And-Variables-Lab/06.ReversedChars/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace _06.ReversedChars
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            string charStr = "";
+
+            for (int i = 1; i <= 3; i++)
+            {
+                char currChar = char.Parse(Console.ReadLine());
+                if (i != 3)
+                {
+                    charStr += currChar + " ";
+                }
+                else
+                    charStr += currChar;
+            }
+            char[] charArray = charStr.ToCharArray();
+            Array.Reverse(charArray);
+
+            foreach (char c in charArray)
+            {
+                Console.Write(c);
+            }
+        }
+    }
+}
