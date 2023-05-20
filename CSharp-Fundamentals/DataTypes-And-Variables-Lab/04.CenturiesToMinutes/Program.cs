d52c2cf0f4637de5638ac3667fdf62290c94e6c5
Adrean Angelov <execthd@gmail.com>
2023-05-20 19:44:07 +0300

    C# Fundamentals Course

---
+++ CSharp-Fundamentals/DataTypes-And-Variables-Lab/04.CenturiesToMinutes/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace _04.CenturiesToMinutes
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            //365.2422
+            int centuriesCount = int.Parse(Console.ReadLine());
+            int years = centuriesCount * 100;
+            int days = (int)(years * 365.2422m);
+            int hours = (int)days * 24;
+            int minutes = hours * 60;
+
+            string output = $"{centuriesCount} centuries = {years} " +
+                            $"years = {days} days = {hours} hours" +
+                            $" = {minutes} minutes";
+
+            Console.WriteLine(output);
+        }
+    }
+}
