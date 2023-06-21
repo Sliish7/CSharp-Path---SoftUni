acd319a4167434d3bcc1c4e6125058f2cee3b106
Adrean Angelov <execthd@gmail.com>
2023-06-22 00:08:18 +0300

    C# Fundamentals Course

---
+++ CSharp-Fundamentals/DataTypes-And-Variables-MoreExercise/05.DecryptingMessage/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace _05.DecryptingMessage
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            int steps = int.Parse(Console.ReadLine());
+            int letters = int.Parse(Console.ReadLine());
+            string word = "";
+
+            for (int i = 1; i <= letters; i++)
+            {
+                char lett = (char)(char.Parse(Console.ReadLine()) + steps);
+                word += lett.ToString();
+            }
+            Console.WriteLine(word);
+        }
+    }
+}
---
+++ CSharp-Fundamentals/DataTypes-And-Variables-MoreExercise/06.BalancedBrackets/Program.cs
@@ -0,0 +1,46 @@
+﻿namespace _06.BalancedBrackets
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            int lines = int.Parse(Console.ReadLine());
+            bool isBalanced = true;
+            bool isOpened = false;
+
+            for (int i = 1; i <= lines; i++)
+            {
+                string input = Console.ReadLine();
+
+                if (isOpened && input == "(" || !isOpened && input == ")")
+                {
+                    isBalanced = false;
+                    break;
+                }
+                if (isOpened && input == "(")
+                {
+                    isBalanced = false;
+                    break;
+                }
+
+                if (input == "(")
+                {
+                    isOpened = true;
+                    isBalanced = false;
+                }
+                else if (input == ")")
+                {
+                    isOpened = false;
+                    isBalanced = true;
+                }
+            }
+
+            if (isBalanced == true)
+            {
+                Console.WriteLine("BALANCED");
+            }
+            else
+                Console.WriteLine("UNBALANCED");
+        }
+    }
+}
