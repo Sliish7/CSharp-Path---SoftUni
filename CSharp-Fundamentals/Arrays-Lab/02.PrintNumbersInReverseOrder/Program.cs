fb7782cb7952726e947dab3fd57d9db8f24b2815
Adrean Angelov <execthd@gmail.com>
2023-06-22 23:21:02 +0300

    C# Fundamentals Course

---
+++ CSharp-Fundamentals/Arrays-Lab/02.PrintNumbersInReverseOrder/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace _02.PrintNumbersInReverseOrder
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            int count = int.Parse(Console.ReadLine());
+            int[] numbers = new int[count];
+            for (int i = 0; i < count; i++)
+            {
+                numbers[i] = int.Parse(Console.ReadLine());
+            }
+
+            Array.Reverse(numbers);
+
+            foreach (int num in numbers)
+            {
+                Console.Write($"{num} ");
+            }
+        }
+    }
+}
---
+++ CSharp-Fundamentals/Arrays-Lab/03.RoundingNumbers/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace _03.RoundingNumbers
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            double[] numbers = Console.ReadLine().Split(" ").Select(double.Parse).ToArray();
+
+            foreach (var el in numbers)
+            {
+                int rounded = (int)Math.Round(el, MidpointRounding.AwayFromZero);
+                Console.WriteLine($"{el} => {rounded}");
+            }
+        }
+    }
+}
