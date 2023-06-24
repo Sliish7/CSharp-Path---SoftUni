b3f6052e14d2041282a8470a56c5b98f3f0eba52
Adrean Angelov <execthd@gmail.com>
2023-06-25 00:20:46 +0300

    C# Fundamentals Course

---
+++ CSharp-Fundamentals/Arrays-Exercise/06.EqualSum/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace _06.EqualSum
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            List<int> numbers = new List<int>(Console.ReadLine().Split(" ").Select(int.Parse).ToList());
+            bool isFound = false;
+
+            for (int i = 0; i < numbers.Count; i++)
+            {
+                int index = i;
+                int leftSum = 0;
+                int rightSum = 0;
+
+                for (int k = 0; k < index; k++)
+                {
+                    leftSum += numbers[k];
+                }
+                for (int j = index + 1; j < numbers.Count; j++)
+                {
+                    rightSum += numbers[j];
+                }
+
+                if (leftSum == rightSum)
+                {
+                    Console.WriteLine(index);
+                    isFound = true;
+                    break;
+                }
+            }
+
+            if (!isFound)
+            {
+                Console.WriteLine("no");
+            }
+        }
+    }
+}
