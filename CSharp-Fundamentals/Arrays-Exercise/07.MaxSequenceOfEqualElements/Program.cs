4fcfccbf04d68fb03348c51468e3c3e6b24ed85a
Adrean Angelov <execthd@gmail.com>
2023-06-26 22:47:16 +0300

    C# Fundamentals Course

---
+++ CSharp-Fundamentals/Arrays-Exercise/07.MaxSequenceOfEqualElements/Program.cs
@@ -0,0 +1,46 @@
+﻿using System.Diagnostics.Metrics;
+
+namespace _07.MaxSequenceOfEqualElements
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            List<int> numbers = new List<int>(Console.ReadLine().Split(" ").Select(int.Parse).ToList());
+            int topNum = 0;
+            int counter = 0;
+
+            foreach (int num in numbers)
+            {
+                int index = numbers.IndexOf(num);
+                int prevNum = index;
+                counter++;
+
+                if (index == 0)
+                {
+                }
+                else
+                    prevNum = num;
+
+                int biggestCounter = 0;
+
+                if (num != prevNum && index != 0)
+                {
+                    counter = 0;
+                }
+
+
+                if (counter > biggestCounter)
+                {
+                    biggestCounter = counter;
+                    topNum = num;
+                }
+
+                Console.WriteLine($"Num {num} prevNum {prevNum} counter {counter} biggestConter{biggestCounter} Top Number for now: {topNum}");
+
+            }
+
+            Console.WriteLine(topNum);
+        }
+    }
+}
