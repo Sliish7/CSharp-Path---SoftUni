05c937757f0b62e13406358e23f9c1d306eb7d2d
Adrean Angelov <execthd@gmail.com>
2023-07-04 02:11:59 +0300

    C# Fundamentals Course

---
+++ CSharp-Fundamentals/Arrays-Exercise/08.MagicSum/Program.cs
@@ -0,0 +1,38 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace _08.MagicSum
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            int[] numbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int magicNumber = int.Parse(Console.ReadLine());
+
+            UniquePairs(numbers, magicNumber);
+        }
+
+        private static void UniquePairs(int[] numbers, int magicNumber)
+        {
+            List<int> pairs = new List<int>();
+            int firstN = 0;
+            int secondN = 0;
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                firstN = numbers[i];
+
+                for (int j = i + 1; j < numbers.Length; j++)
+                {
+                    secondN = numbers[j];
+                    int result = firstN + secondN;
+
+                    if (result == magicNumber)
+                    {
+                        Console.WriteLine($"{firstN} {secondN}");
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ CSharp-Fundamentals/Arrays-Exercise/09.KaminoFactory/Program.cs
@@ -0,0 +1,48 @@
+﻿using System.Transactions;
+
+namespace _09.KaminoFactory
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            int length = int.Parse(Console.ReadLine());
+            string input =string.Empty;
+            int[] bestDNA = {0, 0, 0, 0, 0};
+            int biggestCount = 1;
+            int leftmostIndex = length;
+
+            while ((input = Console.ReadLine()) != "Clone them!")
+            {
+                int[] dna = input.Split(" ").Select(int.Parse).ToArray();
+                int startIndex = length;
+                int count = 0;
+
+                for (int i = 0; i < length; i++)
+                {
+                    if (dna[i] == 1)
+                    {
+                        if (count < biggestCount && startIndex > leftmostIndex)
+                        {
+                            startIndex = i;
+                        }
+                        count++;
+
+                    }
+                    else
+                        count = 0;
+
+                }
+
+                if (count >= biggestCount && startIndex <= leftmostIndex)
+                {
+                    leftmostIndex = startIndex;
+                    biggestCount = count;
+                    bestDNA = dna;
+                }
+            }
+
+            Console.WriteLine(string.Join(" ", bestDNA));
+        }
+    }
+}
