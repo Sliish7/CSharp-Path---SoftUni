c5d9b10a1ce58de5ec62e8f44175bc5f2494b9ca
Adrean Angelov <execthd@gmail.com>
2023-07-05 19:10:13 +0300

    C# Fundamentals Course

---
+++ CSharp-Fundamentals/Methods-Exercise/09.PalindromeIntegers/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace _09.PalindromeIntegers
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            int[] arr = { 2, 3, 4, 5, 6 };
+            int[] arr2 = new int[arr.Length];
+            Array.Copy(arr, arr2, arr.Length);
+            Array.Reverse(arr2);
+
+            string input = Console.ReadLine();
+            while (input != "END")
+            {
+                Console.WriteLine(PalindromeIntegers(input));
+
+                input = Console.ReadLine();
+            }
+        }
+
+        private static bool PalindromeIntegers(string input)
+        {
+            bool isPalindrome = true;
+            int number = int.Parse(input);
+
+            char[] digits = number.ToString().ToCharArray();
+            char[] reversed = new char[digits.Length];
+            Array.Copy(digits, reversed, digits.Length);
+            Array.Reverse(reversed);
+
+            for (int i = 0; i < digits.Length; i++)
+            {
+                if (digits[i] != reversed[i])
+                {
+                    isPalindrome = false;
+                    break;
+                }
+                else
+                    isPalindrome= true;
+            }
+
+            return isPalindrome;
+        }
+    }
+}
