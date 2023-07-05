7e0c6e2e86852a1076b45a6efa29fa6c95c04172
Adrean Angelov <execthd@gmail.com>
2023-07-05 17:15:58 +0300

    C# Fundamentals Course

---
+++ CSharp-Fundamentals/Methods-Exercise/07.NxNMatrix/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace _07.NxNMatrix
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            int filler = int.Parse(Console.ReadLine());
+            Console.WriteLine(NxNMatrix(filler));
+        }
+
+        private static string NxNMatrix(int filler)
+        {
+            string output = string.Empty;
+            for (int i = 0; i < filler; i++)
+            {
+                for (int j = 0; j < filler; j++)
+                {
+                    output += filler + " ";
+                }
+                output += "\n";
+            }
+
+            return output;
+        }
+    }
+}
