b33ecd14e73f131699a666fb23bd91977e854f40
Adrean Angelov <execthd@gmail.com>
2023-07-05 17:00:16 +0300

    C# Fundamentals Course

---
+++ CSharp-Fundamentals/Methods-Exercise/06.MiddleCharacters/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace _06.MiddleCharacters
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            MiddleChars();
+        }
+
+        private static string MiddleChars()
+        {
+            string input = Console.ReadLine();
+            if (input.Length % 2 == 0)
+            {
+                Console.Write(input[input.Length / 2 - 1]);
+            }
+            Console.Write(input[input.Length / 2]);
+
+            return input;
+        }
+    }
+}
