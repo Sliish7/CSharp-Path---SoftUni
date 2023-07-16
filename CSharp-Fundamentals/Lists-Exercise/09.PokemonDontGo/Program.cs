1fdeea98c1f2ccb62d42ed2de4b09cf723ed26d0
Adrean Angelov <execthd@gmail.com>
2023-07-16 13:04:16 +0300

    C# Fundamentals Course

--- CSharp-Fundamentals/Lists-Exercise/09.PokemonDontGo/Program.cs
+++ CSharp-Fundamentals/Lists-Exercise/09.PokemonDontGo/Program.cs
@@ -4,7 +4,46 @@
     {
         static void Main()
         {
-            
+            List<int> pokemons = Console.ReadLine().Split().Select(int.Parse).ToList();
+            int result = 0;
+
+            while (pokemons.Count > 0)
+            {
+                int index = int.Parse(Console.ReadLine());
+                int currValue = 0;
+
+                if (index < 0)
+                {
+                    index = 0;
+                    currValue = pokemons[index];
+                    result += pokemons[index];
+                    pokemons[0] = pokemons[pokemons.Count - 1];
+                }
+                else if (index > pokemons.Count - 1)
+                {
+                    index = pokemons.Count - 1;
+                    currValue = pokemons[index];
+                    result += pokemons[index];
+                    pokemons[pokemons.Count - 1] = pokemons[0];
+                }
+                else
+                {
+                    currValue = pokemons[index];
+                    result += pokemons[index];
+                    pokemons.RemoveAt(index);
+                }
+
+                for (int i = 0; i < pokemons.Count; i++)
+                {
+                    if (pokemons[i] <= currValue)
+                    {
+                        pokemons[i] += currValue;
+                    }
+                    else
+                        pokemons[i] -= currValue;
+                }
+            }
+            Console.WriteLine(result);
         }
     }
 }
---
+++ CSharp-Fundamentals/Lists-Exercise/10.SoftUniCoursePlanning/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _10.SoftUniCoursePlanning
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            
+        }
+    }
+}
