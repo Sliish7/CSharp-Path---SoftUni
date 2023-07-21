83a2d29458410149c378fcae00756f27226beea0
Adrean Angelov <execthd@gmail.com>
2023-07-21 22:20:35 +0300

    C# Fundamentals Course

---
+++ CSharp-Fundamentals/Associative-Arrays-Exercise/02.AMinerTask/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace _02.AMinerTask
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            string input;
+            var resources = new Dictionary<string, int>();
+
+            while ((input = Console.ReadLine()) != "stop")
+            {
+                string resource = input;
+                int quantity = int.Parse(Console.ReadLine());
+
+                if (resources.ContainsKey(resource))
+                {
+                    resources[resource] += quantity;
+                }
+                else
+                    resources[resource] = quantity;
+            }
+
+            foreach (var kvp in resources)
+            {
+                Console.WriteLine($"{kvp.Key} -> {kvp.Value}");
+            }
+        }
+    }
+}
