3b24ee1e3a31261eb5ef1fc7fac9b0f47753cbd5
Adrean Angelov <execthd@gmail.com>
2023-07-22 15:37:11 +0300

    C# Fundamentals Course

---
+++ CSharp-Fundamentals/Associative-Arrays-Exercise/03.Orders/Program.cs
@@ -0,0 +1,62 @@
+﻿using System.Xml.Linq;
+
+namespace _03.Orders
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input;
+            Dictionary<string, Drinks> products = new Dictionary<string, Drinks>();
+
+            while ((input = Console.ReadLine()) != "buy")
+            {
+                string[] arguments = input.Split();
+                string name = arguments[0];
+                double price = double.Parse(arguments[1]);
+                int quantity = int.Parse(arguments[2]);
+
+                Drinks drink = new Drinks(name, price, quantity);
+
+                if (!products.ContainsKey(name))
+                {
+                    products[name] = drink;
+                }
+                else
+                    products[name].Update(price, quantity);
+            }
+
+            foreach (KeyValuePair<string, Drinks> pair in products)
+            {
+                Console.WriteLine(pair.Value);
+            }
+        }
+    }
+
+    public class Drinks
+    {
+
+        public Drinks(string name,double price, int quantity)
+        {
+            Name = name;
+            Price = price;
+            Quantity = quantity;
+        }
+
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public int Quantity { get; set; }
+
+        public void Update(double price, int quantity)
+        {
+            Price = price;
+            Quantity += quantity;
+        }
+        public double TotalPrice => Price * Quantity;
+
+        public override string ToString()
+        {
+            return $"{Name} -> {TotalPrice:F2}";
+        }
+    }
+}
