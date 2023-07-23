ce713407b65f4b88d28de29608ad3cbc9c42f7ec
Adrean Angelov <execthd@gmail.com>
2023-07-23 17:43:19 +0300

    C# Fundamentals Course

---
+++ CSharp-Fundamentals/Associative-Arrays-Exercise/07.CompanyUsers/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace _07.CompanyUsers
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            string input;
+            Dictionary<string, List<string>> companies = new Dictionary<string, List<string>>();
+
+            while ((input = Console.ReadLine()) != "End")
+            {
+                string[] arguments = input.Split("->");
+                string companyName = arguments[0].Trim();
+                string employeeID = arguments[1].Trim();
+
+                if (!companies.ContainsKey(companyName))
+                {
+                    companies.Add(companyName, new List<string>());
+                }
+
+                if (!companies[companyName].Contains(employeeID))
+                {
+                    companies[companyName].Add(employeeID);
+                }
+            }
+
+            foreach (var kvp in companies)
+            {
+                Console.WriteLine($"{kvp.Key}");
+                Console.WriteLine("-- " + string.Join("\n-- ", kvp.Value));
+            }
+        }
+    }
+}
