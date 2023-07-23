2f42c60cb9042c1b940b9dca828599be646d66f1
Adrean Angelov <execthd@gmail.com>
2023-07-23 17:19:06 +0300

    C# Fundamentals Course

--- CSharp-Fundamentals/Associative-Arrays-Exercise/05.Courses/Program.cs
+++ CSharp-Fundamentals/Associative-Arrays-Exercise/05.Courses/Program.cs
@@ -2,9 +2,30 @@
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            Console.WriteLine("Hello, World!");
+            string input;
+            Dictionary<string, List<string>> courses = new Dictionary<string, List<string>>();
+
+            while ((input = Console.ReadLine()) != "end")
+            {
+                string[] arguments = input.Split(":");
+                string course = arguments[0].Trim();
+                string name = arguments[1].Trim();
+
+                if (!courses.ContainsKey(course))
+                {
+                   courses.Add(course, new List<string>());
+                }
+
+                courses[course].Add(name);
+            }
+
+            foreach (var kvp in courses)
+            {
+                Console.WriteLine($"{kvp.Key}: {kvp.Value.Count()}");
+                Console.WriteLine("-- " + string.Join("\n-- ", kvp.Value));
+            }
         }
     }
 }
