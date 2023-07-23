f253778e219d42d210e57c43d2a0071ec8453fdc
Adrean Angelov <execthd@gmail.com>
2023-07-23 17:34:03 +0300

    C# Fundamentals Course

---
+++ CSharp-Fundamentals/Associative-Arrays-Exercise/06.StudentAcademy/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace _06.StudentAcademy
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+            Dictionary<string, List<double>> students = new Dictionary<string, List<double>>();
+
+            for (int i = 0; i < n; i++)
+            {
+                string studentName = Console.ReadLine();
+                double grade = double.Parse(Console.ReadLine());
+
+                if (!students.ContainsKey(studentName))
+                {
+                    students.Add(studentName, new List<double>());
+                }
+                students[studentName].Add(grade);
+            }
+
+            foreach (var kvp in students.Where(x => x.Value.Average() >= 4.50))
+            {
+                Console.WriteLine($"{kvp.Key} -> {kvp.Value.Average():f2}");
+            }
+        }
+    }
+}
