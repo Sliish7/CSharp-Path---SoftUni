3ad45ff9110046b1896ac32704fb138b71ad989d
Adrean Angelov <execthd@gmail.com>
2023-07-26 00:21:10 +0300

    C# Fundamentals Course

---
+++ CSharp-Fundamentals/TextProcessing-Exercise/07.String Explosion/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _07.String_Explosion
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            string input = Console.ReadLine();
+            char[] chars = input.ToCharArray();
+            int strength = 0;
+            for (int i = 0; i < chars.Length; i++)
+            {
+                if (chars[i] == '>')
+                {
+                    strength += chars[i + 1]; 
+                }
+
+                
+            }
+        }
+    }
+}
