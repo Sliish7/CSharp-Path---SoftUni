34179c97e24c747c957106d1c1daed2523143183
Adrean Angelov <execthd@gmail.com>
2023-09-19 22:22:35 +0300

    C# Advanced

---
+++ CSharp-Advanced/StacksAndQueues-Exercise/01.BasicStackOperations/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace _01.BasicStackOperations
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            List<int> arguments = new List<int>(Console.ReadLine().Split().Select(int.Parse).ToList());
+            int numbers = arguments[0];
+            int pops = arguments[1];
+            int target = arguments[2];
+
+            //if found output - print true  | if !found - print smallest | if <stack_count - print 0
+
+            Stack<int> stack = new Stack<int>(Console.ReadLine().Split().Select(int.Parse));
+
+            if (pops < stack.Count)
+            {
+                for (int i = 0; i < pops; i++)
+                {
+                    stack.Pop();
+                }
+
+                if (stack.Contains(target))
+                {
+                    Console.WriteLine(true);
+                }
+                else
+                    Console.WriteLine(stack.Min());
+            }
+            else
+                Console.WriteLine(0);
+
+        }
+    }
+}
