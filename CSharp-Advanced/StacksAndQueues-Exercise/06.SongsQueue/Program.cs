35bcd332227244be44cd305bd4982236187dadef
Adrean Angelov <execthd@gmail.com>
2023-09-20 08:19:21 +0300

    C# Advanced Course

---
+++ CSharp-Advanced/StacksAndQueues-Exercise/06.SongsQueue/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace _06.SongsQueue
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Queue<string> queue = new Queue<string>(Console.ReadLine().Split(", "));
+
+            while (queue.Count > 0)
+            {
+                string command = Console.ReadLine();
+
+                switch (command)
+                {
+                    case "Play": queue.Dequeue(); break;
+
+                    case "Show": Console.WriteLine(string.Join(", ", queue)); break;
+
+                    default:
+                        if (command.StartsWith("Add "))
+                        {
+                            string song = command.Substring(4);
+                            if (!queue.Contains(song))
+                            {
+                                queue.Enqueue(song);
+                            }
+                            else
+                                Console.WriteLine($"{song} is already contained!");
+                        }
+                        break;
+                }
+            }
+            Console.WriteLine("No more songs!");
+        }
+    }
+}
