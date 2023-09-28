ce5ce693cb2b0a01c3cdac016f7a71806aa4ee7f
Adrean Angelov <execthd@gmail.com>
2023-09-29 00:21:25 +0300

    C# Advance Course

---
+++ CSharp-Advanced/Multidimensional-Arrays-Exercise/01.DiagonalDifference/Program.cs
@@ -0,0 +1,44 @@
+﻿using System.Runtime.InteropServices;
+
+namespace _01.DiagonalDifference
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            int size = int.Parse(Console.ReadLine());
+            int[,] matrix = new int[size, size];
+            int primaryDiag = 0;
+            int secondaryDiag = 0;
+
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                int[] inputRow = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    matrix[row, col] = inputRow[col];
+                }
+            }
+
+            for (int row = 0; row < size; row++)
+            {
+                for (int col = 0; col < size; col++)
+                {
+                    if (row == col)
+                    {
+                        primaryDiag += matrix[row, col];
+                    }
+
+                    if (col == size - row - 1)
+                    {
+                        secondaryDiag += matrix[row, col];
+                    }
+                }
+            }
+
+            int result = Math.Abs(primaryDiag - secondaryDiag);
+            Console.WriteLine(result);
+        }
+    }
+}
