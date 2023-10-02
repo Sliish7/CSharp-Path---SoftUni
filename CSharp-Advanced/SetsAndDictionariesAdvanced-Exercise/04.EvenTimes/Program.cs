2d51026a47d5abb5ec6105340d2d21fcdbc87a6f
Adrean Angelov <execthd@gmail.com>
2023-10-02 05:02:24 +0300

    C# Advanced Course

---
+++ CSharp-Advanced/SetsAndDictionariesAdvanced-Exercise/04.EvenTimes/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace _04.EvenTimes
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            int count = int.Parse(Console.ReadLine());
+            Dictionary<int,int> numbers = new Dictionary<int,int>();
+
+            for (int i = 0; i < count; i++)
+            {
+                int number = int.Parse(Console.ReadLine());
+                if (!numbers.ContainsKey(number))
+                {
+                    numbers.Add(number, 1);
+                }
+                else
+                    numbers[number]++;
+            }
+
+            foreach (var kvp in numbers)
+            {
+                if (kvp.Value % 2 == 0)
+                {
+                    Console.WriteLine(kvp.Key);
+                }
+            }
+        }
+    }
+}
