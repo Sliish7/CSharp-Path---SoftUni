5abfde2648ab6360c8aa6e195c3031437b86c4c9
Adrean Angelov <execthd@gmail.com>
2023-10-02 05:47:08 +0300

    C# Advanced Course

---
+++ CSharp-Advanced/SetsAndDictionariesAdvanced-Exercise/05.CountSymbols/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace _05.CountSymbols
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            string input = Console.ReadLine();
+            char[] charArray = input.ToCharArray();
+            SortedDictionary<char, int> chars = new SortedDictionary<char, int>();
+
+            for (int i = 0; i < charArray.Length; i++)
+            {
+                if (!chars.ContainsKey(charArray[i]))
+                {
+                    chars[charArray[i]] = 0;
+                }
+
+                chars[charArray[i]]++;
+            }
+
+            foreach (var kvp in chars)
+            {
+                Console.WriteLine($"{kvp.Key}: {kvp.Value} time/s");
+            }
+        }
+    }
+}
