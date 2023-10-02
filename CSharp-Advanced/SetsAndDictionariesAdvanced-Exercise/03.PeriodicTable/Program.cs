90ae852dadb1e6fd2de518c401d776e3668128f3
Adrean Angelov <execthd@gmail.com>
2023-10-02 04:42:39 +0300

    C# Advanced Course

---
+++ CSharp-Advanced/SetsAndDictionariesAdvanced-Exercise/03.PeriodicTable/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace _03.PeriodicTable
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            int count = int.Parse(Console.ReadLine());
+            SortedSet<string> elements = new SortedSet<string>();
+
+            for (int i = 0; i < count; i++)
+            {
+                List<string> input = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).ToList();
+
+                for (int j = 0; j < input.Count; j++)
+                {
+                    elements.Add(input[j]);
+                }
+            }
+
+            Console.WriteLine(string.Join(" ", elements));
+        }
+    }
+}
