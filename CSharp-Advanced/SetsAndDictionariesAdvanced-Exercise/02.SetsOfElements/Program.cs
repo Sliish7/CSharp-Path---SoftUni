6f22af20155d767e78e89120ee8197e8fef20044
Adrean Angelov <execthd@gmail.com>
2023-10-02 04:31:37 +0300

    C# Advanced Course

---
+++ CSharp-Advanced/SetsAndDictionariesAdvanced-Exercise/02.SetsOfElements/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Xml;
+
+namespace _02.SetsOfElements
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            int[] count = Console.ReadLine().Split(" ").Select(int.Parse).ToArray();
+            HashSet<int> set1 = new HashSet<int>();
+            HashSet<int> set2 = new HashSet<int>();
+            for (int i = 0; i < count[0]; i++)
+            {
+                set1.Add(int.Parse(Console.ReadLine()));
+            }
+
+            for (int i = 0; i < count[1]; i++)
+            {
+                set2.Add(int.Parse(Console.ReadLine()));
+            }
+
+            Console.WriteLine(string.Join(" ", set1.Intersect(set2)));
+        }
+    }
+}
