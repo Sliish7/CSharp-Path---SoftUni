3aa2c210d92b47ceea7d0863967487a037ea3d30
Adrean Angelov <execthd@gmail.com>
2023-10-17 04:27:48 +0300

    C# Advanced Course

---
+++ CSharp-Advanced/FunctionalProgramming-Exercise/06.ReverseAndExclude/Program.cs
@@ -0,0 +1,44 @@
+﻿using System.Reflection;
+
+namespace _06.ReverseAndExclude
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            List<int> numbers = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToList();
+            int n = int.Parse(Console.ReadLine());
+
+            Func<List<int>, List<int>> reverse = (numbers) =>
+            {
+                List<int> result = new List<int>(numbers);
+                result.Reverse();
+
+                return result;
+            };
+
+            Func<List<int>, int, List<int>> exclude = (numbers, n) =>
+            {
+                List<int> result = new();
+
+                foreach (var num in numbers)
+                {
+                    if (num % n == 0)
+                    {
+                        result.Add(num);
+                    }
+                }
+
+                result = reverse(result);
+
+                return result;
+            };
+
+            numbers = exclude(numbers, n);
+            Console.WriteLine(string.Join(" ", numbers));
+        }
+    }
+}
