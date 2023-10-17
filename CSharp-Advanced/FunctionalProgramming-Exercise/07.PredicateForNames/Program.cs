5e2944ccb3f27187f423e8aae1006fdd917a3704
Adrean Angelov <execthd@gmail.com>
2023-10-17 04:40:03 +0300

    C# Advanced Course

---
+++ CSharp-Advanced/FunctionalProgramming-Exercise/07.PredicateForNames/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace _07.PredicateForNames
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+            List<string> names = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .ToList();
+
+            Func<int, List<string>, List<string>> sort = (n, names) =>
+            {
+                List<string> result = new();
+
+                foreach (string name in names)
+                {
+                    if (name.Length <= n)
+                    {
+                        result.Add(name);
+                    }
+                }
+
+                return result;
+            };
+
+            Action<List<string>> print = names => Console.WriteLine(string.Join("\n", names));
+
+            names = sort(n, names);
+            print(names);
+        }
+    }
+}
